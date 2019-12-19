fa8d1d2ff298fbca87d7cbadb338448a5dcd7afc
Chandan Chhouda <chandan@geekyants.com>
2019-12-19 15:32:52 +0530

    Fixed mapping of string[] in codeBits

--- example/codeBits.ts
+++ example/codeBits.ts
@@ -6,53 +6,65 @@ export default {
       start: '',
       end: '',
       children: [
-        'import x from "x";\n',
-        'import x from "x";\n',
-        'import x from "x";\n',
-        'import x from "x";\n',
-        'import x from "x";\n',
+        'import React from "react";\n',
+        'import { View, Text } from "react-native";\n\n',
       ],
     },
     {
-      start: '<View>\n',
-      end: '</View>\n',
+      start: 'class App extends React.Component{\nrender(){\n\treturn(\n',
+      end: '\t\t)\n\t};\n}\n',
       children: [
         {
-          start: '\t<View>\n',
-          end: '\t</View>\n',
+          start: '\t\t<View>\n',
+          end: '\t\t</View>\n',
           children: [
             {
-              start: '\t\t<View>\n',
-              end: '\t\t</View>\n',
+              start: '\t\t\t<View>\n',
+              end: '\t\t\t</View>\n',
               children: [
                 {
-                  start: '\t\t\t<Text>',
-                  end: '</Text>\n',
+                  start: '\t\t\t\t<View>\n',
+                  end: '\t\t\t\t</View>\n',
                   children: [
-                    { start: '', end: '', children: 'Enter some text here' },
+                    {
+                      start: '\t\t\t\t\t<Text>',
+                      end: '</Text>\n',
+                      children: [
+                        {
+                          start: '',
+                          end: '',
+                          children: 'Enter some text here',
+                        },
+                      ],
+                    },
+                    {
+                      start: '\t\t\t\t\t<Text>\n',
+                      end: '\t\t\t\t\t</Text>\n',
+                      children: '\t\t\t\t\t\tEnter some text here\n',
+                    },
                   ],
                 },
+              ],
+            },
+            {
+              start: '\t\t\t<View>\n',
+              end: '\t\t\t</View>\n',
+              children: [
                 {
-                  start: '\t\t\t<Text>\n',
-                  end: '\t\t\t</Text>\n',
-                  children: '\t\t\t\tEnter some text here\n',
+                  start: '\t\t\t\t<Text>\n',
+                  end: '\t\t\t\t</Text>\n',
+                  children: ['\t\t\t\t\tSome other text here\n'],
                 },
               ],
             },
           ],
         },
-        {
-          start: '\t<View>\n',
-          end: '\t</View>\n',
-          children: [
-            {
-              start: '\t\t<Text>\n',
-              end: '\t\t</Text>\n',
-              children: ['\t\t\tSome other text here\n'],
-            },
-          ],
-        },
       ],
+    },
+    {
+      start: '',
+      end: '',
+      children: 'export default App;',
     },
   ],
 };
